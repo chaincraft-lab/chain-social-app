356f20fd8d9608fcfe22f71ccb704ac884f99eaf
ozknsmz <ozknsmz@hotmail.com>
2025-08-15 19:44:07 +0300

    refactor: Update project name and details to reflect 'Haber Sitesi' branding

--- admin-dashboard/src/components/LogoSign/index.tsx
+++ admin-dashboard/src/components/LogoSign/index.tsx
@@ -99,7 +99,7 @@ function Logo() {
 
   return (
     <TooltipWrapper
-      title="Tokyo Free Black Next.js Typescript Admin Dashboard"
+      title="Haber Sitesi Next.js Typescript Admin Dashboard"
       arrow
     >
       <LogoWrapper href="/">
